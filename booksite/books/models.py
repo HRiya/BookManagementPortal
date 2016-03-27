1a6ca3f04ab4e169c4df5bbfb57919056df27740
Adityan J <adityan.jothi@gmail.com>
2016-03-27 23:00:53 +0530

    File uploading possible now

--- booksite/books/models.py
+++ booksite/books/models.py
@@ -6,5 +6,6 @@ from django.db import models
 class Book(models.Model):
     title=models.CharField(max_length=100)
     description=models.CharField(max_length=200)
+    file=models.FileField(upload_to='pdfs',default='')
     def __str__(self):
         return self.title
