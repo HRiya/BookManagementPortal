c4c6ffa7f73bc89acb74d3f53e13c9dc63050223
Adityan J <adityan.jothi@gmail.com>
2016-03-27 22:16:27 +0530

    Initial Book model created

--- booksite/books/models.py
+++ booksite/books/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.CharField(max_length=200)
+    def __str__(self):
+        return self.title
